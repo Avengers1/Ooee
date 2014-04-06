bdd3fcc2b1c65dd8a9f8c5f79aef9b4352934eff
Avengers1 <mateus_vicentin2011@hotmail.com>
2014-04-06 14:57:18 -0300

    Update BotMessage.js

--- BotMessage.js
+++ BotMessage.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 
 //When script loads
-API.sendChat("/me BEM VINDO A ETD (AUTO REGRAS LIGADO) V 1.0 EDITADO POR AVENGERS !")
+API.sendChat("/me BEM VINDO A ETD :v: (AUTO REGRAS LIGADO) V 1.1 EDITADO POR AVENGERS ! :smile: ")
 $('#button-vote-positive').click();
 
 //global var
@@ -27,7 +27,7 @@ var intervalMessage = setInterval(function(){message();},260000);
 
 function message(){
 var m, msgs;
-msgs = ["/me Welcome to Electro, Trap & Dubstep Brasil:  Join the group https://www.facebook.com/groups/ETD.plug/", "/me Música no máximo 5 minutos, quanto menor melhor/ Proibido: Funk, axé, pagode, sertanejo, paródia, rock ,rap, putarias/ Fan for fan liberado/ Entrou na Lista de Espera,o voto é OBRIGATÓRIO, caso contrário, serás retirado da lista de espera/ Não ficar pedindo cargos ",  "/me Cant ask for staff positions/Não peça cargos na STAFF!", "/me Respeite os STAFF da sala/Respect the moderators of the room.", "/me sem spam ou flood no chat/No flooding the chat.", "/me Allowed genres/gêneros permitidos: Electro, Dubstep, Techno, Trap, EDM, Hardstyle , Trance , House", "/me Ola eu falo as regras da sala , porfavor respeite todas as regras ! para nao ser banido da sala! ", "/me Esta com duvidas? peça ajuda a um STAFF/Do you have questions? ask a STAFF", "/me Não crie brigas , ajude sempre a sala ", ];
+msgs = ["/me Welcome to Electro, Trap & Dubstep Brasil:  Join the group https://www.facebook.com/groups/ETD.plug/ :+1: ", "/me Música no máximo 5 minutos, quanto menor melhor/ Proibido: Funk, axé, pagode, sertanejo, paródia, rock ,rap, putarias/ Fan for fan liberado/ Entrou na Lista de Espera,o voto é OBRIGATÓRIO, caso contrário, serás retirado da lista de espera/ Não ficar pedindo cargos :laughing: ",  "/me Cant ask for staff positions/Não peça cargos na STAFF! :thumbsup: ", "/me Respeite os STAFF da sala/Respect the moderators of the room. :angel: ", "/me :warning: sem spam ou flood no chat/No flooding the chat. :warning: ", "/me :warning: Allowed genres/gêneros permitidos: Electro, Dubstep, Techno, Trap, EDM, Hardstyle , Trance , House :warning: ", "/me :thumbsup: Ola eu falo as regras da sala , porfavor respeite todas as regras ! para nao ser banido da sala! :warning:", "/me :question: Esta com duvidas? peça ajuda a um STAFF/Do you have questions? ask a STAFF :question:", "/me Não crie brigas , ajude sempre a sala ", ];
 
 m = Math.floor(Math.random() * msgs.length);
 API.sendChat(msgs[m]);		  
