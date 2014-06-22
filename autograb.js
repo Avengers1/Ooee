97230eece512439dc1325caa7ef59f9640c8e6bd
senhordesativado <mateus_vicentin2011@hotmail.com>
2014-06-21 21:20:04 -0300

    Create autograb.js

---
+++ autograb.js
@@ -0,0 +1,40 @@
+//                           //
+//   AutoGrab para Plug DJ   //
+//                           //
+
+Grab={};
+
+Grab.check = function(e)
+{
+  if(e===null)
+  {
+    API.chatLog("Autograb: Voce e o DJ",true);
+    Grab.djname = null;
+  }
+  else if(API.getDJ().username!=API.getUser().username)
+  {
+    Grab.grab();
+    Grab.djname=API.getDJ().username;
+  }
+  else
+  {
+    if(Grab.djname!=API.getUser().username)
+    {
+      API.chatLog("Autograb: You are now DJ",false);
+      Grab.djname=API.getDJ().username;
+    }
+  }
+};
+Grab.welcome = function()
+{
+  API.chatLog("Autograb by Sonic ON!",true);
+  API.chatLog("Nao de Like", true);
+};
+
+Grab.grab = function()
+{
+  $(".icon-curate").click();
+  $($(".curate").children(".menu").children().children()[0]).mousedown();};
+  Grab.welcome();
+  Grab.check();
+  API.on(API.DJ_ADVANCE,Grab.check);
